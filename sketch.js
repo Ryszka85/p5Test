1eb540bbdd9b8c8b25a2f1dafeec7fc992a87cc0
Ryszka85 <adrian.ryszka@gmx.net>
2019-04-22 22:28:08 +0200

    added comments

--- sketch.js
+++ sketch.js
@@ -1,26 +1,48 @@
-let mouse,test,vel,acc;
+let mousePos,ballPos,velocity,acceleration;
 
 function setup() {
     createCanvas(1265, 648);
     vel = new Vector(0,0);
-test =  new Vector(width/2,height/2);
-acc = new Vector(0,0);
+    ballPos =  new Vector(width/2,height/2);
+    acceleration = new Vector(0,0);
   }
   
   function draw() {
     background(55);
-    mouse = new Vector(mouseX,mouseY);
-    mouse.sub(test.x,test.y);
-    mouse.setMag(0.1);
 
-    acc.x = mouse.x;
-    acc.y = mouse.y;
-    vel.add(acc.x,acc.y);
-    test.add(vel.x,vel.y);
-    ellipse(test.x, test.y, 80, 80);
-    if(vel.x >= 5 || vel.y >= 5){
-        vel.x *= 0.75;
-        vel.y *= 0.75;
+    // creating a mousePos object
+    // initializing object x pos with mouseX and y pos with mouseY
+    mousePos = new Vector(mouseX,mouseY);
+
+    // subtracting mousePos(x,y) with position of the ball´s (x,y)
+    // to create an vector -> from ball to mousePos
+    // to move the ball to the mouse cursors position
+    // with dynamic speed
+    mousePos.sub(ballPos.x,ballPos.y);
+
+    // setting magnitude of that vector to 0,1
+    // for to lower the force(acceleration)
+    mousePos.setMag(0.1);
+
+    // assigning the (x,y) acceleration object
+    // with the vector magnitude(length).
+    acceleration.x = mousePos.x;
+    acceleration.y = mousePos.y;
+
+    // adding acceleration vector to velocity vector(x,y)
+    velocity.add(acceleration.x,acceleration.y);
+
+    // velocity gets added to ballPos
+    // to move the ball in mouse cursor´s direction
+    ballPos.add(velocity.x,velocity.y);
+
+    // draw the ball
+    ellipse(ballPos.x, ballPos.y, 80, 80);
+
+    // checking if velocity is not to high
+    if(velocity.x >= 5 || velocity.y >= 5){
+        velocity.x *= 0.75;
+        velocity.y *= 0.75;
     }
     
 }
