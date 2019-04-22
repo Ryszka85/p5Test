2dd34cd96ac5977088179c07035cb44280c25c1d
Ryszka85 <adrian.ryszka@gmx.net>
2019-04-22 23:02:50 +0200

    seperated files, and readMe description written

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# p5Test
-Testing out p5 stuff
+p5 library playground
+programmer : Ryszka Adrian
+program-name : moving ball towards mouse
+description : A Ball drawn within a canvas accelerates towards
+              the mouse cursor. The acceleration changes by the distance 
+              of the mouse cursor. The distance gets evaluated by calculating
+              a vector which points from the ballPosition(x,y) to the mouseCursor(x,y).
+              In addition to that, the resulting vector gets divided by its magnitude(length)
+              to get a unit vector which finally gets multiplied by a factor to set it s magnitude(lenght)
+              for required acceleration.
--- sketch.js
+++ sketch.js
@@ -1,42 +1,8 @@
 let mousePos,ballPos,velocity,acceleration;
-
- // vector class for coordinates
- class Vector {
-    // initializing x and y coordinates
-  constructor(x,y){
-      this.x = x;
-      this.y = y;
-  }
-  // sub() method subtracts two points A and B or two vectors
-  sub(x,y){
-      this.x -= x;
-      this.y -= y;
-  }
-  // add() method adds two vectors or points A and B
-  add(x,y){
-      this.x += x;
-      this.y += y;
-  }
-  // mag() method returns the magnitude(length) of a vector
-  mag(){
-      return Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2));
-  }
-  // unit() method calculates a unit vector
-  unit(){
-      return {
-          unitX : this.x / this.mag(),
-          unitY : this.y / this.mag()
-      };
-  }
-  // setMag() method sets the magnitude(length) of a vector
-  setMag(n){
-      this.x = this.unit().unitX * n;
-      this.y = this.unit().unitY * n;
-  }
-}
-
+const canvasWidth = 1265;
+const canvasHeight = 648;
 function setup() {
-    createCanvas(1265, 648);
+    createCanvas(canvasWidth, canvasHeight);
     velocity = new Vector(0,0);
     ballPos =  new Vector(width/2,height/2);
     acceleration = new Vector(0,0);
